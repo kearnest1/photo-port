5b9c80ba5e35ea417684373e40e5915065da38ed
Kristin Earnest <kearnest10@gmail.com>
2020-10-21 00:49:00 -0500

    updated files and folders

---
+++ photo-port/src/components/About/_tests_/index.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+
+describe('About component', () => {
+    // First Test
+    it('renders', () => {
+      render(<About />);
+    });
+  
+    // Second Test
+  })
